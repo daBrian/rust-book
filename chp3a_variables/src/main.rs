3100884da4b98bd25db8e553ce2f3cb07f4bf525
Matthias Mahrhofer <matthias.mahrhofer@audi.de>
2026-06-19 15:06:02 +0200

    shadowing
    file:///Users/fvdmiv7/.rustup/toolchains/stable-aarch64-apple-darwin/share/doc/rust/html/book/ch03-01-variables-and-mutability.html#shadowing

---
+++ chp3a_variables/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // 'mut' makes x mutable
+    let mut m = 5;
+    println!("The value of m is: {m}");
+    m = 6;
+    println!("The value of m is: {m}");
+
+    //shadowing
+    let x = 5;
+    println!("The value of x is: {x}");
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of new x is: {x}");
+}
