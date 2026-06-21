9b297cd7efdfde52dc5890328de749729d22bd64
Matthias Mahrhofer <matthias.mahrhofer@audi.de>
2026-06-21 08:54:40 +0200

    chp4-1

---
+++ chp4_ownership/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+
+}
+
+fn c4_1_introduction() {
+    let s1 = String::from("hello");
+
+    let (s2, len) = calculate_length(s1);
+
+    println!("The length of '{s2}' is {len}.");
+    //not valid
+    // println!("The length of '{s1}' is {len}.");
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len(); // len() returns the length of a String
+
+    (s, length)
+}
